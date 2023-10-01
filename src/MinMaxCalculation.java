756a89151b328547d81cebddfef4bda3bb6cde95
anthony <anthony_zalev@hotmail.com>
2023-10-01 16:37:23 -0400

    Initial File Structure for MinMaxCalculation.java

---
+++ src/MinMaxCalculation.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MinMaxCalculation {
+    public MinMaxCalculation(){
+        
+    }
+    public Integer getMinimum(){
+        return -1;
+    }
+    public Integer getMaximum(){
+        return -1;
+    }
+
+
+}
